e010f08e47eedcad04b485d5ae20965808ed0687
Kyaw Khine Htoo <alberthtoo.k@gmail.com>
2025-01-28 15:19:31 +0700

    first commit

---
+++ resources/js/Components/ExpandableMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h6
+      class="py-3 px-4 text-xs font-bold text-gray-600 uppercase rounded-md bg-gray-100 dark:bg-gray-700 shadow-sm cursor-pointer flex justify-between items-center"
+      @click="$emit('toggle')"
+    >
+      <label>{{ label }}</label>
+      <i class="fas transition-transform duration-300" :class="{ 'fa-caret-down': isOpen, 'fa-caret-right': !isOpen }" ></i>
+    </h6>
+    <div v-show="isOpen">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    isOpen: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+h6 {
+  transition: background-color 0.3s, color 0.3s;
+}
+</style>
---
+++ resources/js/Pages/Setup/ActivityLog copy.vue
@@ -0,0 +1,97 @@
+<template>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-white leading-tight">
+        Activity Log
+      </h2>
+    </template>
+
+    <div class="py-2">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+        <div class="bg-white overflow-auto shadow-xl sm:rounded-lg" v-if="activities.data">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  No.
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Log Name</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Description</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Subject Type</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Event</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Subject ID</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Causer Type</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Causer ID</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Properties</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Batch UUID</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Created At</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Updated At</th>
+
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="(row, index) in activities.data " v-bind:key="row.id">
+                <td class="px-6 py-3 whitespace-nowrap">{{ activities.from + index }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.log_name }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.description }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.subject_type }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.event }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.subject_id }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.causer_type }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.causer_id }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.properties }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.batch_uuid }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.created_at }}</td>
+                <td class="px-6 py-3 whitespace-nowrap">{{ row.updated_at }}</td>
+
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
+        <span v-if="activities.links">
+          <pagination class="mt-6" :links="activities.links" />
+        </span>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout';
+import Pagination from '@/Components/Pagination';
+import { reactive, ref, onMounted } from 'vue';
+import { router } from '@inertiajs/vue3'
+export default {
+  name: 'ActivityLog',
+  components: {
+    AppLayout,
+    Pagination
+  },
+  props: {
+    activities: Object,
+    errors: Object
+  },
+  setup(props) {
+
+    onMounted(() => {
+      console.log("Hello");
+    })
+  }
+
+
+
+}
+</script>
